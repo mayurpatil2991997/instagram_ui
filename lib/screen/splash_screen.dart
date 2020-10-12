3d257d5442dc48d90845ac086cf5bd92d88c770d
mayur patil <mayurpatil2991997@gmail.com>
2020-10-12 14:34:01 +0530

    Changed splash screen logo size

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -26,7 +26,7 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Center(
             child: SvgPicture.asset(
               INSTAGRAM_LOGO,
-              height: 65.0,
+              height: 95.0,
             ),
         ),
 
